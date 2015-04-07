5074c507dec29523b3fccde62979c28b2366f691
AlexanderZon <theguitarplayer.am@gmail.com>
2015-04-06 21:55:19 -0430

    email fixes

--- email/index.php
+++ email/index.php
@@ -10,31 +10,34 @@
 	<title></title>
 </head>
 <body>
-	<img src="http://programingphp.hol.es/uploads/<?php echo $_POST['picture-name'] ?>">
+	<img src="http://arsenalindustries.hol.es/uploads/<?php echo $_POST['picture-name'] ?>">
 </body>
 </html>
 
 <?php
 
+	
 	$html = ob_get_contents();
-
-	include('../mailer/PHPMailerAutoload.php');
+// die($html);
+	// include('../mailer/PHPMailerAutoload.php');
+	include('../mailer/class.phpmailer.php');
+	include('../mailer/class.smtp.php');
 
 	$mail = new PHPMailer();
 
 	$mail->isSMTP();                                      // Set mailer to use SMTP
 	$mail->Host = 'mx1.hostinger.co';  // Specify main and backup SMTP servers
 	$mail->SMTPAuth = true;                               // Enable SMTP authentication
-	$mail->Username = 'prueba@programingphp.hol.es';                 // SMTP username
-	$mail->Password = 'TheFourElemelons';                           // SMTP password
-	// $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+	$mail->Username = 'dressroom@arsenalindustries.hol.es';                 // SMTP username
+	$mail->Password = 'ugiS3Btnes';                           // SMTP password
+	$mail->SMTPSecure = 'smtp';                            // Enable TLS encryption, `ssl` also accepted
 	$mail->Port = 2525;                                    // TCP port to connect to
 
-	$mail->From = 'prueba@programingphp.hol.es';
+	$mail->From = 'dressroom@arsenalindustries.hol.es';
 	$mail->FromName = 'Arsenal Industries';
 	$mail->addAddress($_POST['email']);     // Add a recipient
 	// $mail->addAddress('ellen@example.com');               // Name is optional
-	$mail->addReplyTo('info@example.com', 'Information');
+	$mail->addReplyTo('dressroom@arsenalindustries.hol.es', 'Arsenal Industries');
 	// $mail->addCC('cc@example.com');
 	// $mail->addBCC('bcc@example.com');
 
