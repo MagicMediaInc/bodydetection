28b03c26b8afa80e4b6260975b3683967c81de5b
AlexanderZon <theguitarplayer.am@gmail.com>
2015-04-07 22:24:53 -0430

    email save upload image

--- email/index.php
+++ email/index.php
@@ -10,39 +10,50 @@
 	<title></title>
 </head>
 <body>
-	<img src="http://arsenalindustries.hol.es/uploads/<?php echo $_POST['picture-name'] ?>">
+	<img src="../uploads/<?php echo $_POST['picture-name'] ?>">
 </body>
 </html>
 
 <?php
 
-	
 	$html = ob_get_contents();
-// die($html);
-	// include('../mailer/PHPMailerAutoload.php');
-	include('../mailer/class.phpmailer.php');
-	include('../mailer/class.smtp.php');
+
+	$conn = mysqli_connect('localhost','root','', 'arsenal');
+	// $conn = mysqli_connect('SERVER','USER','PASSWORD', 'DATABASE');
+	if($conn):
+		if(isset($_POST['email'])):
+			$data = $_POST;
+			$conn->query("INSERT INTO `arsenal`.`email` (`id`, `email`) VALUES (NULL, '".$data['email']."');")	;
+			#echo true;
+		else:
+			echo false;
+		endif;
+	else:
+		die('No se pudo conectar : ' . mysql_error());
+	endif;
+
+	include('../mailer/PHPMailerAutoload.php');
 
 	$mail = new PHPMailer();
 
 	$mail->isSMTP();                                      // Set mailer to use SMTP
 	$mail->Host = 'mx1.hostinger.co';  // Specify main and backup SMTP servers
 	$mail->SMTPAuth = true;                               // Enable SMTP authentication
-	$mail->Username = 'dressroom@arsenalindustries.hol.es';                 // SMTP username
-	$mail->Password = 'ugiS3Btnes';                           // SMTP password
-	$mail->SMTPSecure = 'smtp';                            // Enable TLS encryption, `ssl` also accepted
+	$mail->Username = 'prueba@programingphp.hol.es';                 // SMTP username
+	$mail->Password = 'TheFourElemelons';                           // SMTP password
+	// $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 	$mail->Port = 2525;                                    // TCP port to connect to
 
-	$mail->From = 'dressroom@arsenalindustries.hol.es';
+	$mail->From = 'prueba@programingphp.hol.es';
 	$mail->FromName = 'Arsenal Industries';
 	$mail->addAddress($_POST['email']);     // Add a recipient
 	// $mail->addAddress('ellen@example.com');               // Name is optional
-	$mail->addReplyTo('dressroom@arsenalindustries.hol.es', 'Arsenal Industries');
+	$mail->addReplyTo('info@example.com', 'Information');
 	// $mail->addCC('cc@example.com');
 	// $mail->addBCC('bcc@example.com');
 
 	// $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-	$mail->addAttachment('../uploads/'.$_POST['picture-name'], 'new.jpg');    // Optional name
+	$mail->addAttachment('../uploads/'.$_POST['picture-name'], 'Mi Chaleco.jpg');    // Optional name
 	$mail->isHTML(true);                                  // Set email format to HTML
 
 	$mail->Subject = 'Mi chaleco en Arsenal Industries';
