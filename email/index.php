02368f03ab7d8c5d3a73e8f3fbbd1fae83c6c2c1
Robert Da Corte <robertdacorte@gmail.com>
2015-04-20 11:22:49 -0430

    Fixing css, add information and logo to the picture

--- email/index.php
+++ email/index.php
@@ -2,23 +2,9 @@
 
 	ob_start();
 
-?>
-
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-	<img src="../uploads/<?php echo $_POST['picture-name'] ?>">
-</body>
-</html>
-
-<?php
-
 	$html = ob_get_contents();
 
-	$conn = mysqli_connect('localhost','root','', 'arsenal');
+	$conn = false; //mysqli_connect('localhost','root','', 'arsenal');
 	// $conn = mysqli_connect('SERVER','USER','PASSWORD', 'DATABASE');
 	if($conn):
 		if(isset($_POST['email'])):
@@ -29,7 +15,7 @@
 			echo false;
 		endif;
 	else:
-		die('No se pudo conectar : ' . mysql_error());
+		//die('No se pudo conectar : ' . mysql_error());
 	endif;
 
 	include('../mailer/PHPMailerAutoload.php');
@@ -37,32 +23,53 @@
 	$mail = new PHPMailer();
 
 	$mail->isSMTP();                                      // Set mailer to use SMTP
-	$mail->Host = 'ssl://smtp.gmail.com';  // Specify main and backup SMTP servers
+	$mail->Host = 'smtp.mailgun.org';                 // Specify main and backup server
 	$mail->SMTPAuth = true;                               // Enable SMTP authentication
 
-	$mail->Username = 'prueba@programingphp.hol.es';                 // SMTP username
-	$mail->Password = 'TheFourElemelons';                           // SMTP password
-	// $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-	$mail->Port = 2525;                                    // TCP port to connect to
+	$mail->Username = 'postmaster@sandboxe2d1ffe3c2fa43e19d2c808826056072.mailgun.org';                 // SMTP username
+	$mail->Password = 'arsenalindustries';                           // SMTP password
+	$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+	$mail->Port = 587;
+	//$mail->SMTPDebug  = 1;                                    // TCP port to connect to
 
-	$mail->From = 'prueba@programingphp.hol.es';
-	$mail->FromName = 'Arsenal Industries';
+	$mail->setFrom ('arsenalindustries@arsenalindustries.com.ve',"Arsenal Industries International");
+	// $mail->FromName = 'Arsenal Industries';
 	$mail->addAddress($_POST['email']);     // Add a recipient
+	$mail->addReplyTo('arsenalindustries@arsenalindustries.com.ve', 'Arsenal Industries International');
 	// $mail->addAddress('ellen@example.com');               // Name is optional
-	$mail->addReplyTo('info@example.com', 'Information');
+	// $mail->addReplyTo('info@example.com', 'Information');
 	// $mail->addCC('cc@example.com');
 	// $mail->addBCC('bcc@example.com');
 
 	// $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-	$mail->addAttachment('../uploads/'.$_POST['picture-name'], 'Mi Chaleco.jpg');    // Optional name
+	
+
+// 	$mail->IsHTML(true);                                  // Set email format to HTML
+
+// $mail->Subject = 'Here is the subject';
+// $mail->Body    = 'This is the HTML message body <strong>in bold!</strong>';
+// $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+// if(!$mail->Send()) {
+//    echo 'Message could not be sent.';
+//    echo 'Mailer Error: ' . $mail->ErrorInfo;
+//    exit;
+// }
+
+// echo 'Message has been sent';
+
+
+	$mail->addAttachment('../uploads/'.$_POST['picture-name'], 'Mi-Chaleco.jpg');    // Optional name
 	$mail->isHTML(true);                                  // Set email format to HTML
 
 	$mail->Subject = 'Mi chaleco en Arsenal Industries';
-	$mail->Body    = $html;
+	$mail->Body    = " El chaleco antibalas debe ser una prenda que brinde un mínimo de protección acorde a las amenazas que se enfrenten en el ámbito cotidiano donde se emplee: El nivel de protección balística debe superar el calibre del arma que porta el usuario, de esta forma, se está protegido ante un eventual ataque con ella. Un chaleco debe Ofrecer Garantía por escrito por parte del Fabricante";
 
 	if(!$mail->send()) {
-	    echo 'Message could not be sent.';
-	    echo 'Mailer Error: ' . $mail->ErrorInfo;
+	    echo 'Tu foto no ha sido enviada, intente nuevamente';
+	    // return false;
 	} else {
-	    echo 'Message has been sent';
+	    // return true;
+	    echo 'Tu foto ha sido enviada!';
 	}
+
